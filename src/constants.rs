ad991294896b0396a823dc6a6a15f9ee4fc3221a
MoritzKn <moritz.kneilmann@gmx.de>
2018-02-05 00:34:21 +0100

    Tweak constansts

--- src/constants.rs
+++ src/constants.rs
@@ -2,11 +2,11 @@ use base_types::*;
 use ggez::graphics::Color;
 
 //--- Grid ---//
-pub const GRID_SIZE: GridVector = GridVector { x: 48, y: 26 };
+pub const GRID_SIZE: GridVector = GridVector { x: 40, y: 22 };
 pub const GRID_TILE_SIZE: f32 = 20.0;
 
 //--- Visual ---//
-pub const APPLE_BLINK_TIME: f64 = 0.5;
+pub const APPLE_BLINK_TIME: f64 = 0.7;
 pub const BLINK_INTERVAL: f64 = 0.08;
 pub const BLINK_LENGTH_AFTER_DEATH: f64 = 0.6;
 pub const COLOR_BACKGROUND: Color = Color {
@@ -23,14 +23,14 @@ pub const COLOR_FOREGROUND: Color = Color {
 };
 pub const FONT_DEFAULT_SIZE: u32 = 22;
 pub const FONT_GAME_OVER_SIZE: u32 = 80;
-pub const GAME_OVER_TIMEOUT: f64 = 0.5;
+pub const GAME_OVER_TIMEOUT: f64 = 0.9;
 pub const GRID_TILE_PADDING: f32 = 1.0;
 pub const INFO_BAR_HIGHT: f32 = 40.0;
 
 //--- Game play ---//
 pub const INITIAL_SIZE: GridUnit = 2;
-pub const INITIAL_SPEED: f64 = 0.06;
-pub const INITIAL_VELOCITY: GridVector = GridVector { x: 0, y: 0 };
+pub const INITIAL_SPEED: f64 = 0.1;
+pub const INITIAL_VELOCITY: GridVector = GridVector { x: 1, y: 0 };
 
-pub const GROW_PER_APPLE: GridUnit = 2;
-pub const SPEED_INCREASE_FRACTION: f64 = 26.0;
+pub const GROW_PER_APPLE: GridUnit = 3;
+pub const SPEED_INCREASE_FRACTION: f64 = 36.0;
